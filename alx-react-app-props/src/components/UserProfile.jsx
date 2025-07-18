e0bc4ebbe4d0165f4368638712a260d459dcb877
Abubakar Ado Garba <abubakaradogarba@gmail.com>
2025-07-18 20:41:16 +0100

    Props update with useContext

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useContext } from 'react';
-import UserContext from '../../UserContext';
+import UserContext from '../contexts/UserContext';
 
  function UserProfile() {
   const userData = useContext(UserContext);
