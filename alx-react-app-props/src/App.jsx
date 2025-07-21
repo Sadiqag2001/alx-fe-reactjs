be06a9fc3225bc4459fbec3cadc5043f7c25253c
Abubakar Ado Garba <abubakaradogarba@gmail.com>
2025-07-21 23:50:08 +0100

    props removed

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,29 +1,36 @@
 
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
-import UserContext from './contexts/UserContext'
+// import WelcomeMessage from './components/WelcomeMessage'
+// import Header from './components/Header'
+// import MainContent from './components/MainContent'
+// import Footer from './components/Footer'
+// import UserProfile from './components/UserProfile'
+import UserContext from './components/UserContext'
+import ProfilePage from './components/ProfilePage';
+
+// function App() {
+//   const userData = {
+//   name: "Abubakar Ado Garba",
+//   age: "23",
+//   bio: "I'm a Computer Engineer and Web Developer."
+// };
+
 
 
 function App() {
-  const userData = {
-  name: "Abubakar Ado Garba",
-  age: "23",
-  bio: "I'm a Computer Engineer and Web Developer."
-};
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
 
   return (
     <UserContext.Provider value={userData}>
-    <>
+    {/* <>
       <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
       <UserProfile /> 
-    </>
+    </> */}
+    <ProfilePage userData={userData} />
     </UserContext.Provider>
   )
 }
