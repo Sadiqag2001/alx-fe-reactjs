49ee1b046a2a5ef34c3ffbad06334c968d82d6f3
Abubakar Ado Garba <abubakaradogarba@gmail.com>
2025-08-08 21:57:24 +0100

    Search functionality

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,22 +1,66 @@
 import React from 'react'
 import { useState } from 'react';
-const Search = () => {
-  const [query, setQuery] = useState('');
+import { fetchUserData } from '../services/githubService';
 
-  const handleSearch = () => {
-    console.log(`Searching for: ${query}`);
+const Search = () => {
+  const [username, setUsername] = useState('');
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!username.trim()) return;
+
+    setLoading(true);
+    setError('');
+    setUser(null);
+
+    try {
+      const data = await fetchUserData(username);
+    } catch (error) {
+      setError('Looks like we cant find the user')
+    } finally{
+      setLoading(false);
+    }
   };
 
   return (
     <div>
-      <input
-        className='border rounded-2xl m-6 py-2 px-8'
-        type="text"
-        placeholder="Search GitHub username..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button className='' onClick={handleSearch}>Search</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          className='border rounded-2xl m-6 py-2 px-8'
+          type="text"
+          placeholder="Search GitHub username..."
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          />
+        <button type='submit' className='text-white bg-[#1a1a1a]'>Search</button>
+      </form>
+       {loading && <p>Loading...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {user && (
+        <div style={{ marginTop: '1rem' }}>
+          <img
+            src={user.avatar_url}
+            alt={user.login}
+            width="100"
+            style={{ borderRadius: '50%' }}
+          />
+          <h2>{user.name || user.login}</h2>
+          <a
+            href={user.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: '#0366d6' }}
+          >
+            View Profile
+          </a>
+        </div>
+      )}
     </div>
   )
 }
