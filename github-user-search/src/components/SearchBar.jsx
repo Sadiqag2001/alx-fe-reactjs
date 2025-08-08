fa2fe94fa2399d3ac62e307237532ce12cb7043a
Abubakar Ado Garba <abubakaradogarba@gmail.com>
2025-08-08 20:33:55 +0100

    modifications with styling

---
+++ github-user-search/src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from 'react';
+const SearchBar = () => {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    console.log(`Searching for: ${query}`);
+  };
+
+  return (
+    <div>
+      <input
+        className='border rounded-2xl m-6 py-2 px-8'
+        type="text"
+        placeholder="Search GitHub username..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button className='' onClick={handleSearch}>Search</button>
+    </div>
+  )
+}
+
+export default SearchBar
