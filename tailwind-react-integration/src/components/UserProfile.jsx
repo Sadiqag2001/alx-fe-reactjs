70eb6c546c855e9e987aaf8fef5e7a36b329f87f
Abubakar Ado Garba <abubakaradogarba@gmail.com>
2025-08-09 13:29:34 +0100

    tailwind integration with responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile() {
     <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
       <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 ">John Doe</h1>
-      <p className="text-gray-600 text-base sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <p className="text-gray-600 sm:text-sm text-base sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
