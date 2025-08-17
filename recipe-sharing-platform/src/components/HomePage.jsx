aaf8e1f3746374055d08196d9c6bd81b6f65f6bf
Abubakar Ado Garba <abubakaradogarba@gmail.com>
2025-08-17 19:27:06 +0100

    update

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -13,8 +13,7 @@ function HomePage() {
     <div className='p-5'>
         <h1 className='text-4xl font-bold'>Recipes</h1>
         <div  className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
-            <div className='shadow-xl rounded-md'>
-
+            <div className='shadow-xl rounded-md hover:shadow-sm hover:translate-y-1'>
             {recipes.map((recipe) => (
             <div key={recipes.id} className=' w-full h-auto shadow flex flex-col'>
                 <img src={recipes.image} alt={recipes.title} className='w-full h-[40%]' />
