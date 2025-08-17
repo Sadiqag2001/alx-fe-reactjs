bea6e84e1749031805160e108f1bc9bf7d7801b0
Abubakar Ado Garba <abubakaradogarba@gmail.com>
2025-08-17 19:24:13 +0100

    update

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import recipesData from "../data.json";
+
+function HomePage() {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(()=> {
+        setRecipes(recipesData)
+    }, [])
+
+  return (
+    <div className='p-5'>
+        <h1 className='text-4xl font-bold'>Recipes</h1>
+        <div  className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+            <div className='shadow-xl rounded-md'>
+
+            {recipes.map((recipe) => (
+            <div key={recipes.id} className=' w-full h-auto shadow flex flex-col'>
+                <img src={recipes.image} alt={recipes.title} className='w-full h-[40%]' />
+                <h1 className='text-2xl'>{recipes.title}</h1>
+                <p className='text-sm'>{recipes.title}</p>
+            </div>
+            ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
